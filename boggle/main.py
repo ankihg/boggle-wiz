df871fea24a2e07d57ff1c9355ce225d4bc96141
Anki <anki@Ankis-MacBook-Pro.local>
2018-11-30 14:42:00 -0800

    track used positions in attempt, must deepcopy used_pos dict so positions used in other attempts arent exluded

--- boggle/main.py
+++ boggle/main.py
@@ -1,3 +1,5 @@
+import copy
+
 class TrieNode:
     def __init__(self, char, is_word=False, children_by_char={}):
         self.char = char
@@ -95,20 +97,27 @@ class Round:
             for col_i in range(0, self.board.num_cols):
                 char = self.board.get_position(row_i, col_i)
                 if self.trie_root.has_next(char):
-                    self._visit_position(char, self.trie_root.get_next(char), row_i, col_i)
+                    self._visit_position(char, self.trie_root.get_next(char), row_i, col_i, {})
         return self.found_words
 
     # TODO exclude previosuly used positions
-    def _visit_position(self, prefix, trie_node, row_i, col_i):
+    def _visit_position(self, prefix, trie_node, row_i, col_i, used_positions):
         if trie_node.is_word:
             self.found_words[prefix] = True
 
+        _mark_position_as_used(used_positions, row_i, col_i)
         for move in self.board.moves:
             next_row_i = move.get_row_i(row_i)
             next_col_i = move.get_col_i(col_i)
-            if move.is_allowed(row_i, col_i) and trie_node.has_next(self.board.get_position(next_row_i, next_col_i)):
+            if move.is_allowed(row_i, col_i) and trie_node.has_next(self.board.get_position(next_row_i, next_col_i)) and (not _is_position_used(used_positions, next_row_i, next_col_i)):
                 next_char = self.board.get_position(next_row_i, next_col_i)
-                self._visit_position(prefix + next_char, trie_node.get_next(next_char), next_row_i, next_col_i)
+                self._visit_position(prefix + next_char, trie_node.get_next(next_char), next_row_i, next_col_i, copy.deepcopy(used_positions))
+
+def _is_position_used(used_positions, row_i, col_i):
+    return str(row_i) + ',' + str(col_i) in used_positions
+
+def _mark_position_as_used(used_positions, row_i, col_i):
+    used_positions[str(row_i) + ',' + str(col_i)] = True
 
 
 round = Round(valid_words, board)
