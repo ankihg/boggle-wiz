0083c796b316bdafedd3b8eaa2f63eb2a21171ed
Anki <anki@Ankis-MacBook-Pro.local>
2018-12-01 10:33:22 -0800

    test setup and .gitignore

---
+++ boggle/tests/trie.py
@@ -0,0 +1,13 @@
+# TO RUN
+# `python3 -m unittest tests.trie` from /boggle dir
+
+import unittest
+
+class TestTrie(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+
+if __name__ == '__main__':
+    unittest.main()
