d73a5d7bb5e45cdd226f9cdcd7c1802f81aaf22d
Anki <anki@Ankis-MacBook-Pro.local>
2018-12-01 11:04:30 -0800

    trie tests complete

--- boggle/tests/trie.py
+++ boggle/tests/trie.py
@@ -2,12 +2,24 @@
 # `python3 -m unittest tests.trie` from /boggle dir
 
 import unittest
-from main import TrieNode
+from trie import build_trie
 
 class TestTrie(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_trie(self):
+        valid_words = [ 'carrot', 'cat', 'cats', 'car', 'cars', 'cell', 'bat' ]
+        trie_root = build_trie(valid_words)
+
+        # car is a valid word
+        self.assertTrue(trie_root.get_next('c').get_next('a').get_next('r').is_word)
+        # carr is not a valid word
+        self.assertFalse(trie_root.get_next('c').get_next('a').get_next('r').get_next('r').is_word)
+        # cat is a valid word
+        self.assertTrue(trie_root.get_next('c').get_next('a').get_next('t').is_word)
+
+        # no valid words start with x
+        self.assertFalse(trie_root.has_next('x'))
+
 
 
 if __name__ == '__main__':
