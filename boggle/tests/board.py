edccf0b1c813a3f81f9ed0e769694c1c27a05acf
Anki <anki@Ankis-MacBook-Pro.local>
2018-12-02 11:55:41 -0800

    test custom board demensios

--- boggle/tests/board.py
+++ boggle/tests/board.py
@@ -6,11 +6,17 @@ from board import Board
 
 class TestBoard(unittest.TestCase):
 
-    def test_board(self):
-        board = Board()
+    def test_default_board(self):
+        board = Board() # defaults to 4 x 4
         self.assertEqual(len(board.matrix), 4)
         for i in range(0, len(board.matrix)):
             self.assertEqual(len(board.matrix[i]), 4)
+
+    def test_custom_board(self):
+        board = Board(6, 5) # has dimension 6 x 5
+        self.assertEqual(len(board.matrix), 6)
+        for i in range(0, len(board.matrix)):
+            self.assertEqual(len(board.matrix[i]), 5)
 
     def test_invalid_requirements_error(self):
         with self.assertRaises(ValueError):
