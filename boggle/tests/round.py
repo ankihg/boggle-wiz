c37aeafe9a739e7e3117c2ec25ea98be36404f1b
Anki <anki@Ankis-MacBook-Pro.local>
2018-12-01 23:39:05 -0800

    simple test for round

---
+++ boggle/tests/round.py
@@ -0,0 +1,31 @@
+# TO RUN
+# `python3 -m unittest tests.round` from /boggle dir
+
+import unittest
+from main import Round
+from board import Board
+
+class TestRound(unittest.TestCase):
+
+    def test_round(self):
+        valid_words = [ 'carrot', 'cat', 'cats', 'car', 'cars', 'cell', 'komo' ]
+        board = Board()
+        board.matrix = [
+            [ 'c', 't', 'r', 's' ],
+            [ 'b', 'a', 'r', 'i' ],
+            [ 'b', 'o', 'k', 'o' ],
+            [ 't', 'a', 'r', 'm' ],
+        ]
+        round = Round(valid_words, board)
+        found_words = round.play()
+
+        self.assertTrue('cat' in found_words)
+        self.assertTrue('car' in found_words)
+        self.assertTrue('cars' in found_words)
+        self.assertTrue('carrot' in found_words)
+        self.assertFalse('komo' in found_words)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
