9392f67c034e88a1d053e2a88150077e06b4372f
Anki <anki@Ankis-MacBook-Pro.local>
2018-12-02 11:42:18 -0800

    pretty prints for main script

--- boggle/main.py
+++ boggle/main.py
@@ -1,9 +1,22 @@
+import time
 from board import Board
 from round import Round
 
-
+print('INITIALIZING THE BOARD ...')
 board = Board()
+print('BUILDING VALID WORDS TRIE ...')
 round = Round(False, board)
+
+print('\n')
 print('BOARD')
 round.board.print()
-print(round.solve())
+print('\n')
+
+start_time = time.time()
+found_words_dict = round.solve()
+end_time = time.time()
+time_elapsed = end_time - start_time
+
+found_words = list(found_words_dict.keys())
+print(len(found_words), 'WORDS FOUND IN', time_elapsed, 'SECONDS')
+print(found_words)
