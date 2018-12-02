40116625d69ee68f6b87bdd1ee4b0a25974f421f
Anki <anki@Ankis-MacBook-Pro.local>
2018-12-01 23:15:26 -0800

    board success testing

--- boggle/tests/board.py
+++ boggle/tests/board.py
@@ -6,8 +6,11 @@ from board import Board
 
 class TestBoard(unittest.TestCase):
 
-    # def test_board(self):
-    #
+    def test_board(self):
+        board = Board()
+        self.assertEqual(len(board.matrix), 4)
+        for i in range(0, len(board.matrix)):
+            self.assertEqual(len(board.matrix[i]), 4)
 
     def test_invalid_requirements_error(self):
         with self.assertRaises(ValueError):
