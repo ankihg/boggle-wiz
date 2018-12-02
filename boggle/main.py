c24e7615c70d6301ef33716d93a9782ebce646e4
Anki <anki@Ankis-MacBook-Pro.local>
2018-12-02 11:52:59 -0800

    cleaner prints

--- boggle/main.py
+++ boggle/main.py
@@ -2,13 +2,13 @@ import time
 from board import Board
 from round import Round
 
-print('INITIALIZING THE BOARD ...')
+print('> INITIALIZING THE BOARD ...')
 board = Board()
-print('BUILDING VALID WORDS TRIE ...')
+print('> BUILDING VALID WORDS TRIE ...')
 round = Round(False, board)
 
 print('\n')
-print('BOARD')
+print('> HERE IS THE BOARD')
 round.board.print()
 print('\n')
 
@@ -18,5 +18,5 @@ end_time = time.time()
 time_elapsed = end_time - start_time
 
 found_words = list(found_words_dict.keys())
-print(len(found_words), 'WORDS FOUND IN', time_elapsed, 'SECONDS')
+print('>', len(found_words), 'WORDS FOUND IN', time_elapsed, 'SECONDS')
 print(found_words)
